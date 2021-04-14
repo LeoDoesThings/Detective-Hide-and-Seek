001161924d152f89c7c2eba985b9d8c5796cd255
LeoDT <leodoesthings@outlook.com>
2021-04-14 13:59:54 +1200

    'DRY' clues
    Clues no longer repeat themselves.
    
    No changes to the first clue so I know the format when I write the GUI.

--- DHS-tkinter.py
+++ DHS-tkinter.py
@@ -12,37 +12,39 @@ class Clues:
     seen = []
 
     forest1 = 'You notice a muddy footprint on the ground...'
-    forest2 = 'You notice some dirt on the pavement...'
-    forest3 = 'You notice a stick on the ground...'
-    forest4 = 'You notice a leaf blowing past you...'
+    forest2 = 'some dirt on the pavement'
+    forest3 = 'a stick on the ground'
+    forest4 = 'a leaf blowing past you'
 
-    park1 = 'You notice a branch on the ground...'
-    park2 = 'You notice a stick on the ground...'
-    park3 = ''
-    park4 = ''
+    park1 = 'a branch on the ground'
+    park2 = forest3
+    park3 = forest2
+    park4 = 'some gardening gloves on the ground'
 
-    cafe1 = 'You notice a coffee cup on the ground...'
-    cafe2 = 'You notice a lid on the ground...'
-    cafe3 = 'You notice some liquid on the ground...'
-    cafe4 = 'You notice some drops on the ground...'
+    cafe1 = 'a coffee cup on the ground'
+    cafe2 = 'a lid on the ground'
+    cafe3 = 'some liquid on the ground'
+    cafe4 = 'some drops on the ground'
 
-    house1 = ''
-    house2 = ''
-    house3 = ''
-    house4 = ''
+    house1 = 'a bag on the ground
+    house2 = 'a wallet on the ground'
+    house3 = 'a book on the ground'
+    house4 = cafe4
+    house5 = 'an HP laptop charger on the ground'
 
-    school1 = ''
-    school2 = ''
-    school3 = ''
-    school4 = ''
+    school1 = house3
+    school2 = house1
+    school3 = cafe3
+    school4 = house2
+    school5 = house5
 
-    garden1 = ''
-    garden2 = ''
-    garden3 = ''
+    garden1 = 'some gardening gloves on the ground'
+    garden2 = 'a stick on the ground'
+    garden3 = forest2
     garden4 = ''
 
-    chapel1 = ''
-    chapel2 = ''
+    chapel1 = 'a rosary on the ground'
+    chapel2 = school1
     chapel3 = ''
     chapel4 = ''
 
